59782db94e187590790308837372ff2420ac1f6f
thedefidude18 <102023568+thedefidude18@users.noreply.github.com>
2024-01-13 16:32:23 +0000

    new updates

--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -16,6 +16,8 @@ function Coins({ coins }) {
     const [searchText, setSearchText] = useState("")
     const [page, setPage] = useState(1);
 
+    const [TableData, setTableData] = useState([])
+
     const useStyles = makeStyles({
         row: {
             backgroundColor: "#16171a",
@@ -42,10 +44,24 @@ function Coins({ coins }) {
         );
     };
 
+    useEffect(() => {
+        
+        axios("https://rollupradar.xyz/")
+        .then((res)=>{
+           
+            if(res.status == 200){
+                console.log(res.data.data);
+                setTableData(res.data.data);
+            }
+        })
+    }, [])
+    
+
+
     return (
         <div>
             <div className='rounded-div'>
-                <div className='sm:hidden'>
+            <div className='sm:hidden'>
                     <div className='border-b dark:border-opacity-12'>
                         <nav className='flex space-x-8'>
                             <a href='javascript:void(0);' className='rounded-a text-sm font-medium border-b-2 border-transparent py-3'>
@@ -70,55 +86,50 @@ function Coins({ coins }) {
                     <input onChange={(e) => setSearchText(e.target.value)} type="text" className='bg-gray-100 dark:bg-gray-600 rounded-md py-1 indent-3 outline-none my-5' placeholder='Search' />
                 </div>
 
+               
+
                 <div className='relative'>
                     <div className='overflow-x-auto'>
                         <table className='w-full border-collapse text-center'>
                             <thead className='dark:text-white h-11 dark:bg-181818 '>
                                 <tr className='border-b border-t'>
-                                    <th></th>
+                                  
                                     <th className='text-sm dark:text-hsla-dark whitespace-nowrap'>#</th>
-                                    <th className='text-left text-sm dark:text-hsla-dark whitespace-nowrap'>Rollups</th>
-                                    <th></th>
-                                    <th className='text-sm dark:text-hsla-dark whitespace-nowrap'>Status</th>
-                                    <th className='text-sm dark:text-hsla-dark w-17 whitespace-nowrap'>Stack</th>
-                                    <th className='text-sm dark:text-hsla-dark w-17 whitespace-nowrap'>TVL</th>
-                                    <th className='text-sm dark:text-hsla-dark whitespace-nowrap'>RPC</th>
-                                    <th className='text-sm dark:text-hsla-dark whitespace-nowrap'>Bridge</th>
-                                    <th className='text-sm dark:text-hsla-dark w-17 whitespace-nowrap'>Github</th>
+                                    <th className='text-left text-sm dark:text-hsla-dark whitespace-nowrap'>🚀Rollups</th>
+                                   
+                                    <th className='text-sm dark:text-hsla-dark whitespace-nowrap'>👨🏻‍💻Status</th>
+                                    <th className='text-sm dark:text-hsla-dark w-17 whitespace-nowrap'>🗂️Stack</th>
+                                    <th className='text-sm dark:text-hsla-dark w-17 whitespace-nowrap'>📊TVL</th>
+                                    <th className='text-sm dark:text-hsla-dark whitespace-nowrap'>🔗 RPC</th>
+                                    <th className='text-sm dark:text-hsla-dark whitespace-nowrap'>🌉Bridge</th>
+                                    <th className='text-sm dark:text-hsla-dark w-17 whitespace-nowrap'>🌐Github</th>
                                 </tr>
                             </thead>
                             <tbody className='dark:text-white'>
-                                {coins.filter((value) => {
-                                    if (searchText === "") {
-                                        return value
-                                    } else if (value.name.toLowerCase().includes(searchText.toLowerCase())) {
-                                        return value
-                                    }
-                                })
-                                    .slice((page - 1) * 20, (page - 1) * 20 + 20)
-                                    .map((coin) => (
-                                        <CoinItem key={coin.id} coin={coin} />
-                                    ))}
+                                {
+                                    TableData.map((ele)=>{
+                                        return(
+                                            <tr  className='h-[75px] border-b overflow-hidden'>
+                                                 <td className='w-40 text-sm py-1 px-2'>{ele.id}</td>
+                                                 <td className='w-40 text-sm py-1 px-2'>{ele.Rollup}</td>
+                                                 <td className='w-40 text-sm py-1 px-2'>{ele.Status}</td>
+                                                 <td className='w-40 text-sm py-1 px-2'>{ele.Stack}</td>
+                                                 <td className='w-40 text-sm py-1 px-2'>{ele.TVL}</td>
+                                                 <td className='w-40 text-sm py-1 px-2'> <a target='_blank' href={"http://"+ele.RPC}>{ele.RPC}</a> </td>
+                                                 <td className='w-40 text-sm py-1 px-2'> <a target='_blank' href={"http://"+ele.Bridge}>{ele.Bridge}</a> </td>
+                                                 <td className='w-40 text-sm py-1 px-2'> <a target='_blank' href={"http://"+ele.Github}>{ele.Github}</a> </td>
+                                              
+                                          </tr>
+                                        )
+                                    })
+                                }
                             </tbody>
                         </table>
                     </div>
                 </div>
             </div>
 
-            <Pagination
-                count={parseInt((handleSearch()?.length / 20).toFixed(0))}
-                style={{
-                    padding: 20,
-                    width: "100%",
-                    display: "flex",
-                    justifyContent: "center",
-                }}
-                classes={{ ul: classes.pagination }}
-                onChange={(_, value) => {
-                    setPage(value);
-                    window.scroll(0, 450);
-                }}
-            />
+          
         </div>
 
     );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,10 +10,9 @@ function Footer() {
         <footer className='bg-footer-dark'>
             <div className='bg-3f3f3f text-center py-4 px-0'>
                 <div className='w-140 max-w-full my-0 mx-auto py-0 px-4'>
-                    <i className='text-e15241 mr-1'><FontAwesomeIcon icon={faTriangleExclamation} /></i>
-                    <a href='javascript:void(0);' className='text-e15241 text-sm hover:text-a71d2a'>중요 면책조항:</a>&nbsp;
+                    <i className='text-e15241 mr-1'>🚀</i>
                     <span className='text-eee text-sm'>
-                        당사의 웹사이트, 참조 사이트, 관련 응용 프로그램, 포럼, 블로그, 소셜 미디어 계정 및 기타 플랫폼("사이트")에 제공되는 정보는 타사 소스로부터 입수되었으며 귀하에게 일반적인 정보제공을 목적으로만 제공됩니다. 당사는 당사의 콘텐츠에 어떠한 것도 보증하지 않습니다(정확성 및 최신성을 포함하되 이에 국한되지 않음). 당사가 제공한 콘텐츠의 어떠한 부분도 귀하가 어떤 목적으로든 특정하게 의존할 수 있는 재무적 조언, 법적 조언 또는 기타 모든 형태의 조언을 구성하지 않습니다. 당사의 콘텐츠에 대한 모든 사용 또는 의존은 귀하 자신의 판단에 의한 것이며, 귀하는 당사의 콘텐츠에 의존하기 전에 스스로 조사, 검토, 분석, 검증을 수행해야 합니다. 거래는 큰 손실이 발생할 수 있는 위험한 활동입니다. 따라서 결정을 내리기 전에 본인의 재무 관리자와 상의하세요. 당사 사이트의 모든 콘텐츠는 권유 또는 제안을 위한 것이 아닙니다.
+                    RollupRadar gives you useful insights and analytics on L1, L2 and L3 Rollups in Blockchain.
                     </span>
                 </div>
             </div>
@@ -22,24 +21,26 @@ function Footer() {
                 <div className='my-5 grid gap-2 md:grid-cols-4 sm:grid-cols-3'>
                     <div>
                         <h1 className='text-2xl font-semibold'>RollupRadar</h1>
-                        <p className='max-w-[160px] mt-2 text-sm'>고객 센터 | <a href="javascript:void(0);">버그 바운티 |</a><br /> 면책 조항 | <a href="javascript:void(0);">이용 약관 |</a > 개인정보취급방침 |</p>
-                    </div>
+                        <li className='text-4a4a4a dark:text-eee'>
+                                    <a href='javascript:void(0);' className='text-sm font-medium hover:text-green-hover'>+ List your rollup</a>
+                                </li>
+                        </div>
                     <div>
-                        <h2 className='text-xl font-semibold mb-2'>둘러보기</h2>
+                        <h2 className='text-xl font-semibold mb-2'>Docs</h2>
                         <ul className=' text-gray-400'>
-                            <li>비트코인 가격</li>
-                            <li>이더리움 가격</li>
-                            <li>DeFi 코인</li>
-                            <li>메타버스 코인</li>
+                        <li className='text-4a4a4a dark:text-eee'>
+                                    <a href='javascript:void(0);' className='text-sm font-medium hover:text-green-hover'>+ List your rollup</a>
+                                </li>
+                       
                         </ul>
                     </div>
                     <div>
-                        <h2 className='text-xl font-semibold mb-2'>리소스</h2>
+                        <h2 className='text-xl font-semibold mb-2'>Partner with us</h2>
                         <ul className=' text-gray-400'>
-                            <li>영구계약</li>
-                            <li>암호화폐 뉴스</li>
-                            <li>비트코인 보유고</li>
-                            <li>암호화폐 히트맵</li>
+                            <li><li className='text-4a4a4a dark:text-eee'>
+                                    <a href='javascript:void(0);' className='text-sm font-medium hover:text-green-hover'>+ List your rollup</a>
+                                </li></li>
+                           
                         </ul>
                     </div>
                     <div className='flex flex-col'>
@@ -65,11 +66,11 @@ function Footer() {
 
             <div className='py-2 1lg:hidden'>
                 <div className='flex justify-center items-center'>
-                    <a href='javascript:void(0);' className='text-white text-xs'>면책조항</a>
+                    <a href='#' className='text-white text-xs'>+ List your Rollup</a>
                     <span className='mx-1 text-4a4a4a'>•</span>
-                    <a href='javascript:void(0);' className='text-white text-xs'>이용약관</a>
+                    <a href='#' className='text-white text-xs'>이용약관</a>
                     <span className='mx-1 text-4a4a4a'>•</span>
-                    <a href='javascript:void(0);' className='text-white text-xs'>개인정보취급방침</a>
+                    <a href='#;' className='text-white text-xs'>개인정보취급방침</a>
                 </div>
                 <div className='flex justify-center items-center mt-2 text-xs text-4a4a4a'>
                     © 2024 RollupRadar. All Rights Reserved.
