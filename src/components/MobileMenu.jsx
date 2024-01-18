a6c8a98fe28f9668cb3fb2dc6119dfb12d5c0091
thedefidude18 <102023568+thedefidude18@users.noreply.github.com>
2024-01-18 13:30:29 +0100

    Update MobileMenu.jsx

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -8,14 +8,14 @@ function MobileMenu({user, signOut}) {
   return (
     <Menu right>
             <Link to="/">Home</Link>
-            <Link to="/"><a href='https://forms.gle/PjEsUKf4Lb7mziPU6' className='text-white text-xs'><button className='text-xs px-3 py-1 bg-slate-700 rounded-lg'>+ List your Rollup</button></a>Explorer</Link>
-            <Link to="/">Explorer</Link>
-            <Link to="/">Explorer</Link>
+            <Link to="/"><a href='https://forms.gle/PjEsUKf4Lb7mziPU6' className='text-white text-xs'><button className='text-xs px-3 py-1 bg-slate-700 rounded-lg'>+ List your Rollup</button></a></Link>
+            <Link to="/">RaaS</Link>
+            <Link to="/">Docs</Link>
             {user?.email && <Link to="/account">Account</Link>}
             {user?.email ? (<button onClick={signOut}>Log Out</button>
             ) : (
             <Link to="/signin">
-                Sign In
+                Connect
             </Link>)}
         
         <Switcher/>
